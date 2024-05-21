2f8bc09ad6e4cbfd83ac5bd0dd14fe65a817a617
Raphael Muniz <raphael@copin.ufcg.edu.br>
2024-05-21 09:17:40 -0300

    tailwind configuration

--- config/settings/base.py
+++ config/settings/base.py
@@ -83,8 +83,6 @@ THIRD_PARTY_APPS = [
     "corsheaders",
     "drf_spectacular",
     "rest_framework_simplejwt",
-    "tailwind",
-    "theme",
     "django_browser_reload",
 ]
 
@@ -98,11 +96,6 @@ LOCAL_APPS = [
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
-
-# TAILWIND CSS
-# ------------------------------------------------------------------------------
-# https://django-tailwind.readthedocs.io/en/latest/installation.html
-TAILWIND_APP_NAME = "theme"
 
 INTERNAL_IPS = [
     "127.0.0.1",
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./sistema_pei/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
