d7aa648b2060b2056852d5ec9aff3a2f61b64b28
Raphael Muniz <raphael@copin.ufcg.edu.br>
2024-05-28 21:07:29 -0300

    Atualizar configuração do Tailwind para adicionar animação de fade-out

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./sistema_pei/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        fade: 'fadeOut 5s ease-in-out',
+      },
+      keyframes: theme => ({
+        fadeOut: {
+          '0%': { opacity: '1' },
+          '100%': { opacity: '0'},
+        },
+      }),
+    },
   },
   plugins: [],
 }
