20710421ec8a7ba83fb2f6367ad6143985fd4062
Raphael Muniz <raphael.muniz@ifrn.edu.br>
2024-07-19 11:45:26 -0300

    Atualizações nos modelos e administração de disciplinas e ofertas

--- sistema_pei/academics/admin.py
+++ sistema_pei/academics/admin.py
@@ -7,7 +7,7 @@ from . import models
 class SubjectAdmin(admin.ModelAdmin):
     search_fields = ["iteraction_text"]
     readonly_fields = ["updated_by", "created_at"]
-    list_display = ["name", "subject_type", "course", "teacher"]
+    list_display = ["name", "subject_type"]
 
 
 class CoursesAdmin(admin.ModelAdmin):
@@ -17,4 +17,4 @@ class CoursesAdmin(admin.ModelAdmin):
 
 
 admin.site.register(models.Subject, SubjectAdmin)
-admin.site.register(models.Courses, CoursesAdmin)
+admin.site.register(models.Course, CoursesAdmin)
