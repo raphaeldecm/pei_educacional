3313ec2474cfd8d995a624b1eff507f97079e61c
WillianM19 <willianm1928@gmail.com>
2024-09-09 11:03:04 -0300

    feat: Listar ofertas por máterias selecionada, ano e semestre atuais

--- sistema_pei/academics/filters.py
+++ sistema_pei/academics/filters.py
@@ -1,4 +1,5 @@
 import django_filters
+from django.utils import timezone
 from django.db.models import Q
 
 from sistema_pei.academics.models import Enrollment
@@ -8,18 +9,26 @@ from sistema_pei.academics.models import Offer
 class OfferFilter(django_filters.FilterSet):
     search = django_filters.CharFilter(method="filter_by_search", label="Search")
 
+    year = django_filters.NumberFilter(
+        field_name="year",
+        lookup_expr="exact",
+        label="Ano",
+    )
+    
+    semester = django_filters.ChoiceFilter(
+        field_name="semester",
+        choices=Offer.Semester.choices,
+        label="Semestre",
+    )
+
     class Meta:
         model = Offer
-        fields = {
-            "teacher": ["exact"],
-            "subject": ["exact"],
-        }
+        fields = ["teacher", "subject", "semester", "year"]
 
     def filter_by_search(self, queryset, name, value):
         return queryset.filter(
             Q(subject__name__icontains=value) | Q(teacher__name__icontains=value),
         )
-
 
 class EnrollmentFilter(django_filters.FilterSet):
     search = django_filters.CharFilter(
