816c2aaddc32f6c47898df7c1313a11cbe93461f
WillianM19 <willianm1928@gmail.com>
2024-09-27 13:35:14 -0300

    feat(PEI): Adicionando filtros

--- sistema_pei/academics/filters.py
+++ sistema_pei/academics/filters.py
@@ -87,18 +87,26 @@ class SubjectFilter(django_filters.FilterSet):
 
 
 class PeiFilter(django_filters.FilterSet):
-    # search = django_filters.CharFilter(
-    #     field_name="name",
-    #     lookup_expr="icontains",
-    #     label="Search",
-    # )
+    search = django_filters.CharFilter(method='multi_field_search', label="Search")
 
-    # courses = django_filters.ModelChoiceFilter(
-    #     queryset=models.Course.objects.all(),
-    #     field_name="courses",
-    #     label="Courses",
-    # )
+    course = django_filters.ModelChoiceFilter(
+        queryset=models.Course.objects.all(),
+        field_name='enrollment__offer__course',
+        label="course"
+    )
+
+    subject = django_filters.ModelChoiceFilter(
+        queryset=models.Subject.objects.all(),
+        field_name='enrollment__offer__subject',
+        label="subject"
+    )
 
     class Meta:
         model = Pei
-        fields = []
+        fields = ['status']
+
+    def multi_field_search(self, queryset, name, value):
+        return queryset.filter(
+            Q(enrollment__student__name__icontains=value) |
+            Q(enrollment__student__registration__icontains=value)
+        )
