0ed83a838a6ff12714f58f31990a9cd2dbd733ac
Raphael Muniz <raphael.muniz@ifrn.edu.br>
2024-10-16 09:36:56 -0300

    Refatora administração de professores e atualiza email do administrador

--- sistema_pei/users/management/commands/create_admin_user.py
+++ sistema_pei/users/management/commands/create_admin_user.py
@@ -6,7 +6,7 @@ User = get_user_model()
 
 class Command(BaseCommand):
     ADMIN_NAME = "admin"
-    ADMIN_EMAIL = "raphael.muniz@ifrn.edu.br"
+    ADMIN_EMAIL = "admin@nadic.ifrn.edu.br"
     ADMIN_PASSWORD = (
         "argon2$argon2id$v=19$m=102400,t=2,p=8$S09uUzdDRDFPSWlDN2R3O"  # noqa: S105
         "GJ6cWFvWQ$dCZshwPCs0FDEET8dc+KznlGxkZhwLMTtKGoWiB2JYo"
