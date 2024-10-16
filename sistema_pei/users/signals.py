6f2fc1f71a72986f49ccbe90ffb3bbd77fae2062
Raphael Muniz <raphael.muniz@ifrn.edu.br>
2024-10-16 11:18:34 -0300

    Refatora sinais de login para verificar grupo de usuário
    Adiciona um novo arquivo signals.py em sistema_pei/users para implementar um sinal que é acionado quando um usuário faz login. O sinal verifica se o usuário pertence a algum grupo e, caso contrário, faz logout, exibe uma mensagem de erro e redireciona para a página de login.
    
    Co-authored-by: ifrn-pei/feature/export-pei

---
+++ sistema_pei/users/signals.py
@@ -0,0 +1,19 @@
+from allauth.account.signals import user_logged_in
+from django.contrib import messages
+from django.contrib.auth import logout
+from django.dispatch import receiver
+from django.shortcuts import redirect
+
+
+@receiver(user_logged_in)
+def check_user_group(sender, request, user, **kwargs):
+
+    if not user.groups.exists():
+        logout(request)
+        messages.error(
+            request,
+            "Você não pertence ao grupo necessário para acessar o sistema."
+            "Entre em contato com o administrador.",
+        )
+        return redirect("account_login")
+    return None
