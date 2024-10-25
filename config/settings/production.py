04888126ff03042897a3cbeb767337b53588f315
Kaio Dantas <kaio@aba.ifrn.local>
2024-10-25 10:58:42 -0300

    update para subir com https utilizando production.py

--- config/settings/production.py
+++ config/settings/production.py
@@ -13,7 +13,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["pei.com"])
 CSRF_TRUSTED_ORIGINS = env.list("DJANGO_CSRF_TRUSTED_ORIGINS", default=["http://localhost:8000"])
 
-DEBUG = True
+DEBUG = False
 
 # DATABASES
 # ------------------------------------------------------------------------------
