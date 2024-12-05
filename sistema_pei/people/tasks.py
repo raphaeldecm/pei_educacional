898e537308efb16352316d501b3e4aedd4d23cc5
WillianM19 <willianm1928@gmail.com>
2024-12-05 11:53:20 -0300

    fix: Corrigindo erros pós-merge

--- sistema_pei/people/tasks.py
+++ sistema_pei/people/tasks.py
@@ -7,9 +7,7 @@ from celery.exceptions import CeleryError
 
 from sistema_pei.educational_plan.models import Pei
 
-from .models import Pei, Teacher
-from sistema_pei.people.models import Notification
-from sistema_pei.people.tasks import notify_teacher_email
+from sistema_pei.people.models import Notification, Teacher
 from sistema_pei.educational_plan.utils import NotificationEmailContent
 
 from celery import shared_task
