523dbab65bf7a58abfc2c14e97534d8cddcde7a9
WillianM19 <willianm1928@gmail.com>
2025-01-20 20:59:39 -0300

    fix: adiciona novo campo para envio de imagem de perfil do professor

--- sistema_pei/people/models.py
+++ sistema_pei/people/models.py
@@ -72,7 +72,17 @@ class Teacher(Person):
         null=True,
         related_name="teachers",
     )
-    photo = models.ImageField(upload_to="teachers", verbose_name=_("Foto"))
+    photo = models.URLField(
+        verbose_name=_("Foto"),
+        blank=True,
+        max_length=constants.URL_LENGTH,
+    )
+    photoAlt = models.ImageField(
+        upload_to="teachers",
+        blank=True,
+        null=True,
+        verbose_name=_("Foto alternativa")
+    )
     code = models.CharField(
         verbose_name=_("Matrícula"),
         max_length=constants.SMALL_CHAR_FIELD_NAME_LENGTH,
