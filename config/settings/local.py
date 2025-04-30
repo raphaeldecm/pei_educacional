a0e11ce39ece0b0a49fb3937e1f841adb84ceece
Raphael Muniz <raphael.muniz@ifrn.edu.br>
2025-04-30 15:04:02 -0300

    feat(settings): Atualizar ALLOWED_HOSTS e CSRF_TRUSTED_ORIGINS para produção

--- config/settings/local.py
+++ config/settings/local.py
@@ -13,9 +13,19 @@ SECRET_KEY = env(
     default="BGS9KbnTSEYXiRZq0xkie2Xiu81XpjK7EM8wjcaOYkwY0Aa3e1ay4cgMs4aYK4FY",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "pei-hmg.nadic.ifrn.edu.br"]  # noqa: S104
+ALLOWED_HOSTS = [
+    "localhost",
+    "0.0.0.0",
+    "127.0.0.1",
+    "10.21.0.125",
+    "pei-hmg.nadic.ifrn.edu.br",
+    "pei.ifrn.edu.br",
+]  # noqa: S104
 
-CSRF_TRUSTED_ORIGINS = ["https://pei-hmg.nadic.ifrn.edu.br"]
+CSRF_TRUSTED_ORIGINS = [
+    "https://pei-hmg.nadic.ifrn.edu.br",
+    "https://pei.ifrn.edu.br",
+]
 
 # CACHES
 # ------------------------------------------------------------------------------
