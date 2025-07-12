5486389e04ba0e302d7b9224fba7cad7f0f78ebb
Guilherme <jg21504956@gmail.com>
2025-07-12 15:29:24 -0300

    Trocando os selects com varias opções por pesquisas com opções

--- sistema_pei/core/templatetags/search_option.py
+++ sistema_pei/core/templatetags/search_option.py
@@ -4,12 +4,22 @@ from django.template import Library
 register = Library()
 
 #tag
+
+
 @register.inclusion_tag(
     'templatetags/_search_option.html',
     takes_context=True
 )
 def search_option(context,object_name,query,input_placeholder=''):
-    current_query = context.request.GET.get(object_name)
+    """
+        inclui o componente mais o contexto necessario pra a apesquisa
+        É NECESSARIO USAR O load_search_script PARA CARREGAR AS FUNÇÕES JS PRINCIPAIS
+    """
+    current_query = None
+    
+    if context.request.GET.get(object_name):
+        current_query = int(context.request.GET.get(object_name))
+        
     current_input_value = query.filter(id=current_query).first()
     
     return {
