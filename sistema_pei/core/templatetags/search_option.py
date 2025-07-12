891d2498b2f399729057f2d7fbcb6a52bafe60c2
Guilherme <jg21504956@gmail.com>
2025-07-12 14:37:12 -0300

    finalizando a tag de pesquisa

--- sistema_pei/core/templatetags/search_option.py
+++ sistema_pei/core/templatetags/search_option.py
@@ -8,14 +8,18 @@ register = Library()
     'templatetags/_search_option.html',
     takes_context=True
 )
-def search_option(context,input_name,query,input_placeholder=''):
-    current_query = context.request.GET.get(input_name)
+def search_option(context,object_name,query,input_placeholder=''):
+    current_query = context.request.GET.get(object_name)
     current_input_value = query.filter(id=current_query).first()
     
     return {
         'current_query': current_query,
         'current_input_value' : current_input_value,
-        'input_name' : input_name,
+        'object_name' : object_name,
         'input_placeholder' : input_placeholder,
         'query' : query
     }
+
+@register.inclusion_tag('templatetags/scripts/_search_script.html')
+def load_search_script():
+    return 
