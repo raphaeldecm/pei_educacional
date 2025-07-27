7f04fcaa6cdd27a6dacab24d975d3782844b6952
Guilherme <jg21504956@gmail.com>
2025-07-27 16:15:01 -0300

    Retirando o is_delete_button do action_item

--- sistema_pei/core/templatetags/action.py
+++ sistema_pei/core/templatetags/action.py
@@ -23,14 +23,13 @@ def action_buttons(detail_view,edit_view,object,model_name,**kwargs):
 @register.inclusion_tag(
     'components/_action_item.html',
 )
-def action_item(message,default_icon,hover_icon,border_class,hover_class,is_delete_button=False,link='#',link_args=''): 
+def action_item(message,default_icon,hover_icon,border_class,hover_class,link='#',link_args=''): 
     return  {
         'message': message,
         'default_icon': default_icon,
         'hover_icon': hover_icon,
         'border_color': border_class,
         'hover_color': hover_class,
-        'is_delete_button': is_delete_button,
         'link' : link,
         'link_args' : link_args
     }   
