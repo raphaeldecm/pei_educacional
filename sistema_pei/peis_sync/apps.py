5657113567dc776ddf879c1ff571bf0c44e2207c
WillianM19 <willianm1928@gmail.com>
2025-07-28 15:11:17 -0300

    feat(peis-sync): adiciona verbose names, e outros ajustes

--- sistema_pei/peis_sync/apps.py
+++ sistema_pei/peis_sync/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class PeiSyncConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'sistema_pei.peis_sync'
+    verbose_name = "📱 Aplicativo PEIs Sync"
--- sistema_pei/peis_sync/models.py
+++ sistema_pei/peis_sync/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
 class AppVersion(models.Model):
-    version = models.CharField(max_length=20)
-    releaseDate = models.DateTimeField()
+    version = models.CharField(max_length=20, verbose_name="Versão")
+    releaseDate = models.DateTimeField(verbose_name="Data de lançamento")
+    apkFile = models.FileField(upload_to="peis-sync-apks/", blank=True, verbose_name="Arquivo de instalação APK")
 
     class Meta:
         ordering = ['-releaseDate']
+        verbose_name = "Versão do App"
+        verbose_name_plural = "Versões do App"
 
     def __str__(self):
         return self.version
