f47afdf3223c14f6fb4abc2bc75883985c4316c1
WillianM19 <willianm1928@gmail.com>
2025-07-28 15:13:02 -0300

    feat(peis-sync): adiciona validação no envio de arquivos apk

--- sistema_pei/peis_sync/models.py
+++ sistema_pei/peis_sync/models.py
@@ -1,9 +1,15 @@
 from django.db import models
+from django.core.validators import FileExtensionValidator
 
 class AppVersion(models.Model):
     version = models.CharField(max_length=20, verbose_name="Versão")
     releaseDate = models.DateTimeField(verbose_name="Data de lançamento")
-    apkFile = models.FileField(upload_to="peis-sync-apks/", blank=True, verbose_name="Arquivo de instalação APK")
+    apkFile = models.FileField(
+        upload_to="peis-sync-apks/",
+        blank=True,
+        verbose_name="Arquivo de instalação APK",
+        validators=[FileExtensionValidator(allowed_extensions=['apk'])]
+    )
 
     class Meta:
         ordering = ['-releaseDate']
