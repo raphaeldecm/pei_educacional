46e52291138ff3edb11d7bc30d9fa26f1daf0647
Raphael Muniz <raphael.muniz@ifrn.edu.br>
2025-07-31 15:45:11 -0300

    feat: adiciona EMAIL_HOST_USER e EMAIL_HOST_PASSWORD nas configurações de email

--- config/settings/base.py
+++ config/settings/base.py
@@ -254,7 +254,6 @@ EMAIL_PORT = env("DJANGO_EMAIL_PORT", default=587)
 EMAIL_USE_SSL = env("DJANGO_EMAIL_USE_SSL", default=False)
 EMAIL_USE_TLS = env("DJANGO_EMAIL_USE_TLS", default=True)
 SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
-EMAIL_HOST_USER = env("DJANGO_EMAIL_HOST_USER", default="nao-responder@mail.com")
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-timeout
 EMAIL_TIMEOUT = 5
