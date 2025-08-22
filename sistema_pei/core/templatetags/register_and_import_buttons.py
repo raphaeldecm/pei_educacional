7e793f711ee4ed108ee6f3a06f0bb6d09fcf995e
Guilherme <jg21504956@gmail.com>
2025-08-22 20:41:38 -0300

    Adicionando o componente de import

--- sistema_pei/core/templatetags/register_and_import_buttons.py
+++ sistema_pei/core/templatetags/register_and_import_buttons.py
@@ -11,3 +11,27 @@ def register_button(text,link):
         'button_text': text,
         'link': reverse(link),
     }
+    
+    
+@register.inclusion_tag(
+    'components/_import_button.html'
+)    
+def import_button(text,link):
+    return {
+        'button_text': text,
+        'link': reverse(link),
+    }
+    
+
+@register.inclusion_tag(
+    'templatetags/register_import_buttons.html'
+)    
+def register_and_import_buttons(register_text,register_link,import_text,import_link):
+    return {
+        'register_text' : register_text,
+        'register_link': register_link,
+        'import_text' : import_text,
+        'import_link': import_link,
+    }
+    
+
