cf69b52b4a7a3101786c245c010e10b60bf1a4b8
Raphael Muniz <raphael.muniz@ifrn.edu.br>
2025-10-14 09:24:05 -0300

    remove comandos desnecessários do script de carregamento de dados do banco

--- sistema_pei/core/management/commands/load_db_data.py
+++ sistema_pei/core/management/commands/load_db_data.py
@@ -11,11 +11,8 @@ class CommandItem(NamedTuple):
 
 
 COMMANDS = [
-    CommandItem("migrate", "Migrating database..."),
     CommandItem("update_user_profiles", "Updating user profiles..."),
     CommandItem("create_admin_user", "Creating superuser..."),
-    CommandItem("load_db_people", "Loading people data..."),
-    # CommandItem("populate_courses", "Loading courses data..."),
 ]
 
 
